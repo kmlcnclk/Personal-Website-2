1045905223574fea98245a7d1bf20827ec5f973a
KamilcanCelik <kamilcancelik5757@gmail.com>
2021-06-22 13:40:32 +0300

    personal-website-2

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,14 +37,17 @@ export default function Home({
           content="Cristiano, Ronaldo, Cristiano Ronaldo, Cr7, cr7"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:url" content="https://cristianoronaldo.vercel.app" />
+        <meta
+          property="og:url"
+          content="https://cristiano-ronaldo.vercel.app"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Cristiano Ronaldo" />
         <meta property="og:description" content="Cristiano Ronaldo" />
         <meta name="twitter:card" content="summary" />
         <meta
           name="twitter:url"
-          content="https://cristianoronaldo.vercel.app"
+          content="https://cristiano-ronaldo.vercel.app"
         />
         <meta name="twitter:title" content="Cristiano Ronaldo" />
         <meta name="twitter:description" content="Cristiano Ronaldo" />
