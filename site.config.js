4a1a2dd737f07eb66ccc9057b9fe0f5007394244
Mr.Steelcan <mr.steelcan@gmail.com>
2022-02-03 12:09:57 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,15 +5,12 @@ module.exports = {
     emails: 'https://www.instagram.com/mr.steelcan/',
   },
   social: {
-    unsplash: 'https://unsplash.com/@ademilter',
-    vsco: 'https://vsco.co/adem/gallery',
+    unsplash: 'https://unsplash.com/',
     instagram: 'https://www.instagram.com/cristiano/',
-    twitter: 'https://twitter.com/ademilter',
-    medium: 'https://medium.com/@ademilter',
-    github: 'https://github.com/ademilter',
-    youtube: 'https://youtube.com/ademilter',
+    twitter: 'https://twitter.com/',
+    medium: 'https://medium.com/',
+    github: 'https://github.com/KamilcanCelik',
+    youtube: 'https://youtube.com/',
     feyz: 'https://t.me/feyzli',
-    superpeer: 'https://superpeer.com/adem',
   },
 };
-//https://instagram.com/ademilter  
